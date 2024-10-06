b432ee6e938a4db33d333a225f6c2ea5cb03e4c0
Yuri <yuri0x7c1@gmail.com>
2024-10-06 12:47:34 +0300

    Update for bs-layout 1.0.3

--- src/main/java/com/github/yuri0x7c1/vaadin/addon/demo/BsLayoutView.java
+++ src/main/java/com/github/yuri0x7c1/vaadin/addon/demo/BsLayoutView.java
@@ -7,15 +7,13 @@ import org.vaadin.addons.yuri0x7c1.bslayout.BsColumn.Size;
 import org.vaadin.addons.yuri0x7c1.bslayout.BsLayout;
 import org.vaadin.addons.yuri0x7c1.bslayout.BsRow;
 
-import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.H3;
-import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.html.Pre;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
-@CssImport("./styles.css")
 @Route(value = "", layout = MainLayout.class)
 public class BsLayoutView extends VerticalLayout {
 
@@ -26,24 +24,24 @@ public class BsLayoutView extends VerticalLayout {
 			"Equal-width",
 			new BsLayout().withRows(
 				new BsRow().withColumns(
-					new BsColumn(new Label("1 of 2")).withSize(Size.XS),
-					new BsColumn(new Label("2 of 2")).withSize(Size.XS)
+					new BsColumn(new Span("1 of 2")).withSize(Size.XS),
+					new BsColumn(new Span("2 of 2")).withSize(Size.XS)
 				),
 				new BsRow().withColumns(
-					new BsColumn(new Label("1 of 3")).withSize(Size.XS),
-					new BsColumn(new Label("2 of 3")).withSize(Size.XS),
-					new BsColumn(new Label("3 of 3")).withSize(Size.XS)
+					new BsColumn(new Span("1 of 3")).withSize(Size.XS),
+					new BsColumn(new Span("2 of 3")).withSize(Size.XS),
+					new BsColumn(new Span("3 of 3")).withSize(Size.XS)
 				)
 			),
 			"new BsLayout().withRows(\n"
 			+ "	new BsRow().withColumns(\n"
-			+ "		new BsColumn(new Label(\"1 of 2\")).withSize(Size.XS),\n"
-			+ "		new BsColumn(new Label(\"2 of 2\")).withSize(Size.XS)\n"
+			+ "		new BsColumn(new Span(\"1 of 2\")).withSize(Size.XS),\n"
+			+ "		new BsColumn(new Span(\"2 of 2\")).withSize(Size.XS)\n"
 			+ "	),\n"
 			+ "	new BsRow().withColumns(\n"
-			+ "		new BsColumn(new Label(\"1 of 3\")).withSize(Size.XS),\n"
-			+ "		new BsColumn(new Label(\"2 of 3\")).withSize(Size.XS),\n"
-			+ "		new BsColumn(new Label(\"3 of 3\")).withSize(Size.XS)\n"
+			+ "		new BsColumn(new Span(\"1 of 3\")).withSize(Size.XS),\n"
+			+ "		new BsColumn(new Span(\"2 of 3\")).withSize(Size.XS),\n"
+			+ "		new BsColumn(new Span(\"3 of 3\")).withSize(Size.XS)\n"
 			+ "	)\n"
 			+ ");"
 		);
@@ -52,26 +50,26 @@ public class BsLayoutView extends VerticalLayout {
 			"Setting one column width",
 			new BsLayout().withRows(
 				new BsRow().withColumns(
-					new BsColumn(new Label("1 of 3")).withSize(Size.XS),
-					new BsColumn(new Label("2 of 3")).withSize(Size.XS, 6),
-					new BsColumn(new Label("3 of 3")).withSize(Size.XS)
+					new BsColumn(new Span("1 of 3")).withSize(Size.XS),
+					new BsColumn(new Span("2 of 3")).withSize(Size.XS, 6),
+					new BsColumn(new Span("3 of 3")).withSize(Size.XS)
 				),
 				new BsRow().withColumns(
-					new BsColumn(new Label("1 of 3")).withSize(Size.XS),
-					new BsColumn(new Label("2 of 3")).withSize(Size.XS, 5),
-					new BsColumn(new Label("3 of 3")).withSize(Size.XS)
+					new BsColumn(new Span("1 of 3")).withSize(Size.XS),
+					new BsColumn(new Span("2 of 3")).withSize(Size.XS, 5),
+					new BsColumn(new Span("3 of 3")).withSize(Size.XS)
 				)
 			),
 			"new BsLayout().withRows(\n"
 			+ "	new BsRow().withColumns(\n"
-			+ "		new BsColumn(new Label(\"1 of 3\")).withSize(Size.XS),\n"
-			+ "		new BsColumn(new Label(\"2 of 3\")).withSize(Size.XS, 6),\n"
-			+ "		new BsColumn(new Label(\"3 of 3\")).withSize(Size.XS)\n"
+			+ "		new BsColumn(new Span(\"1 of 3\")).withSize(Size.XS),\n"
+			+ "		new BsColumn(new Span(\"2 of 3\")).withSize(Size.XS, 6),\n"
+			+ "		new BsColumn(new Span(\"3 of 3\")).withSize(Size.XS)\n"
 			+ "	),\n"
 			+ "	new BsRow().withColumns(\n"
-			+ "		new BsColumn(new Label(\"1 of 3\")).withSize(Size.XS),\n"
-			+ "		new BsColumn(new Label(\"2 of 3\")).withSize(Size.XS, 5),\n"
-			+ "		new BsColumn(new Label(\"3 of 3\")).withSize(Size.XS)\n"
+			+ "		new BsColumn(new Span(\"1 of 3\")).withSize(Size.XS),\n"
+			+ "		new BsColumn(new Span(\"2 of 3\")).withSize(Size.XS, 5),\n"
+			+ "		new BsColumn(new Span(\"3 of 3\")).withSize(Size.XS)\n"
 			+ "	)\n"
 			+ ");"
 		);
@@ -83,26 +81,26 @@ public class BsLayoutView extends VerticalLayout {
 			"All breakpoints",
 			new BsLayout().withRows(
 				new BsRow().withColumns(
-					new BsColumn(new Label("col")).withSize(Size.XS),
-					new BsColumn(new Label("col")).withSize(Size.XS),
-					new BsColumn(new Label("col")).withSize(Size.XS),
-					new BsColumn(new Label("col")).withSize(Size.XS)
+					new BsColumn(new Span("col")).withSize(Size.XS),
+					new BsColumn(new Span("col")).withSize(Size.XS),
+					new BsColumn(new Span("col")).withSize(Size.XS),
+					new BsColumn(new Span("col")).withSize(Size.XS)
 				),
 				new BsRow().withColumns(
-					new BsColumn(new Label("col-8")).withSize(Size.XS, 8),
-					new BsColumn(new Label("col-4")).withSize(Size.XS, 4)
+					new BsColumn(new Span("col-8")).withSize(Size.XS, 8),
+					new BsColumn(new Span("col-4")).withSize(Size.XS, 4)
 				)
 			),
 			"new BsLayout().withRows(\n"
 			+ "	new BsRow().withColumns(\n"
-			+ "		new BsColumn(new Label(\"col\")).withSize(Size.XS),\n"
-			+ "		new BsColumn(new Label(\"col\")).withSize(Size.XS),\n"
-			+ "		new BsColumn(new Label(\"col\")).withSize(Size.XS),\n"
-			+ "		new BsColumn(new Label(\"col\")).withSize(Size.XS)\n"
+			+ "		new BsColumn(new Span(\"col\")).withSize(Size.XS),\n"
+			+ "		new BsColumn(new Span(\"col\")).withSize(Size.XS),\n"
+			+ "		new BsColumn(new Span(\"col\")).withSize(Size.XS),\n"
+			+ "		new BsColumn(new Span(\"col\")).withSize(Size.XS)\n"
 			+ "	),\n"
 			+ "	new BsRow().withColumns(\n"
-			+ "		new BsColumn(new Label(\"col-8\")).withSize(Size.XS, 8),\n"
-			+ "		new BsColumn(new Label(\"col-4\")).withSize(Size.XS, 4)\n"
+			+ "		new BsColumn(new Span(\"col-8\")).withSize(Size.XS, 8),\n"
+			+ "		new BsColumn(new Span(\"col-4\")).withSize(Size.XS, 4)\n"
 			+ "	)\n"
 			+ ");"
 		);
@@ -113,37 +111,37 @@ public class BsLayoutView extends VerticalLayout {
 			new BsLayout().withRows(
 				// Stack the columns on mobile by making one full-width and the other half-width
 				new BsRow().withColumns(
-					new BsColumn(new Label("xs-12 md-8")).withSize(Size.XS, 12).withSize(Size.MD, 8),
-					new BsColumn(new Label("xs-6 md-4")).withSize(Size.XS, 6).withSize(Size.MD, 4)
+					new BsColumn(new Span("xs-12 md-8")).withSize(Size.XS, 12).withSize(Size.MD, 8),
+					new BsColumn(new Span("xs-6 md-4")).withSize(Size.XS, 6).withSize(Size.MD, 4)
 				),
 				// Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop
 				new BsRow().withColumns(
-					new BsColumn(new Label("xs-6 md-4")).withSize(Size.XS, 6).withSize(Size.MD, 4),
-					new BsColumn(new Label("xs-6 md-4")).withSize(Size.XS, 6).withSize(Size.MD, 4),
-					new BsColumn(new Label("xs-6 md-4")).withSize(Size.XS, 6).withSize(Size.MD, 4)
+					new BsColumn(new Span("xs-6 md-4")).withSize(Size.XS, 6).withSize(Size.MD, 4),
+					new BsColumn(new Span("xs-6 md-4")).withSize(Size.XS, 6).withSize(Size.MD, 4),
+					new BsColumn(new Span("xs-6 md-4")).withSize(Size.XS, 6).withSize(Size.MD, 4)
 				),
 				// Columns are always 50% wide, on mobile and desktop
 				new BsRow().withColumns(
-					new BsColumn(new Label("xs-6")).withSize(Size.XS, 6),
-					new BsColumn(new Label("xs-6")).withSize(Size.XS, 6)
+					new BsColumn(new Span("xs-6")).withSize(Size.XS, 6),
+					new BsColumn(new Span("xs-6")).withSize(Size.XS, 6)
 				)
 			),
 			"new BsLayout().withRows(\n"
 			+ "	// Stack the columns on mobile by making one full-width and the other half-width\n"
 			+ "	new BsRow().withColumns(\n"
-			+ "		new BsColumn(new Label(\"xs-12 md-8\")).withSize(Size.XS, 12).withSize(Size.MD, 8),\n"
-			+ "		new BsColumn(new Label(\"xs-6 md-4\")).withSize(Size.XS, 6).withSize(Size.MD, 4)\n"
+			+ "		new BsColumn(new Span(\"xs-12 md-8\")).withSize(Size.XS, 12).withSize(Size.MD, 8),\n"
+			+ "		new BsColumn(new Span(\"xs-6 md-4\")).withSize(Size.XS, 6).withSize(Size.MD, 4)\n"
 			+ "	),\n"
 			+ "	// Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop\n"
 			+ "	new BsRow().withColumns(\n"
-			+ "		new BsColumn(new Label(\"xs-6 md-4\")).withSize(Size.XS, 6).withSize(Size.MD, 4),\n"
-			+ "		new BsColumn(new Label(\"xs-6 md-4\")).withSize(Size.XS, 6).withSize(Size.MD, 4),\n"
-			+ "		new BsColumn(new Label(\"xs-6 md-4\")).withSize(Size.XS, 6).withSize(Size.MD, 4)\n"
+			+ "		new BsColumn(new Span(\"xs-6 md-4\")).withSize(Size.XS, 6).withSize(Size.MD, 4),\n"
+			+ "		new BsColumn(new Span(\"xs-6 md-4\")).withSize(Size.XS, 6).withSize(Size.MD, 4),\n"
+			+ "		new BsColumn(new Span(\"xs-6 md-4\")).withSize(Size.XS, 6).withSize(Size.MD, 4)\n"
 			+ "	),\n"
 			+ "	// Columns are always 50% wide, on mobile and desktop\n"
 			+ "	new BsRow().withColumns(\n"
-			+ "		new BsColumn(new Label(\"xs-6\")).withSize(Size.XS, 6),\n"
-			+ "		new BsColumn(new Label(\"xs-6\")).withSize(Size.XS, 6)\n"
+			+ "		new BsColumn(new Span(\"xs-6\")).withSize(Size.XS, 6),\n"
+			+ "		new BsColumn(new Span(\"xs-6\")).withSize(Size.XS, 6)\n"
 			+ "	)\n"
 			+ ");"
 		);
@@ -154,28 +152,28 @@ public class BsLayoutView extends VerticalLayout {
 			"Offset classes",
 			new BsLayout().withRows(
 				new BsRow().withColumns(
-					new BsColumn(new Label("md-4")).withSize(Size.MD, 4),
-					new BsColumn(new Label("md-4 offset-md-4")).withSize(Size.MD, 4).withOffset(Offset.MD, 4)
+					new BsColumn(new Span("md-4")).withSize(Size.MD, 4),
+					new BsColumn(new Span("md-4 offset-md-4")).withSize(Size.MD, 4).withOffset(Offset.MD, 4)
 				),
 				new BsRow().withColumns(
-					new BsColumn(new Label("md-3 offset-md-3")).withSize(Size.MD, 3).withOffset(Offset.MD, 3),
-					new BsColumn(new Label("md-3 offset-md-3")).withSize(Size.MD, 3).withOffset(Offset.MD, 3)
+					new BsColumn(new Span("md-3 offset-md-3")).withSize(Size.MD, 3).withOffset(Offset.MD, 3),
+					new BsColumn(new Span("md-3 offset-md-3")).withSize(Size.MD, 3).withOffset(Offset.MD, 3)
 				),
 				new BsRow().withColumn(
-					new BsColumn(new Label("md-6 offset-md-3")).withSize(Size.MD, 6).withOffset(Offset.MD, 3)
+					new BsColumn(new Span("md-6 offset-md-3")).withSize(Size.MD, 6).withOffset(Offset.MD, 3)
 				)
 			),
 			"new BsLayout().withRows(\n"
 			+ "	new BsRow().withColumns(\n"
-			+ "		new BsColumn(new Label(\"md-4\")).withSize(Size.MD, 4),\n"
-			+ "		new BsColumn(new Label(\"md-4 offset-md-4\")).withSize(Size.MD, 4).withOffset(Offset.MD, 4)\n"
+			+ "		new BsColumn(new Span(\"md-4\")).withSize(Size.MD, 4),\n"
+			+ "		new BsColumn(new Span(\"md-4 offset-md-4\")).withSize(Size.MD, 4).withOffset(Offset.MD, 4)\n"
 			+ "	),\n"
 			+ "	new BsRow().withColumns(\n"
-			+ "		new BsColumn(new Label(\"md-3 offset-md-3\")).withSize(Size.MD, 3).withOffset(Offset.MD, 3),\n"
-			+ "		new BsColumn(new Label(\"md-3 offset-md-3\")).withSize(Size.MD, 3).withOffset(Offset.MD, 3)\n"
+			+ "		new BsColumn(new Span(\"md-3 offset-md-3\")).withSize(Size.MD, 3).withOffset(Offset.MD, 3),\n"
+			+ "		new BsColumn(new Span(\"md-3 offset-md-3\")).withSize(Size.MD, 3).withOffset(Offset.MD, 3)\n"
 			+ "	),\n"
 			+ "	new BsRow().withColumn(\n"
-			+ "		new BsColumn(new Label(\"md-6 offset-md-3\")).withSize(Size.MD, 6).withOffset(Offset.MD, 3)\n"
+			+ "		new BsColumn(new Span(\"md-6 offset-md-3\")).withSize(Size.MD, 6).withOffset(Offset.MD, 3)\n"
 			+ "	)\n"
 			+ ");"
 		);
@@ -186,13 +184,13 @@ public class BsLayoutView extends VerticalLayout {
 			"Reordering columns",
 			new BsLayout(
 				new BsRow(
-					new BsColumn(new Label("1-st, but 3-rd on mobile"))
+					new BsColumn(new Span("1-st, but 3-rd on mobile"))
 						.withOrder(Order.XS, 3)
 						.withOrder(Order.SM, 1),
-					new BsColumn(new Label("2-nd"))
+					new BsColumn(new Span("2-nd"))
 						.withOrder(Order.XS, 2)
 						.withOrder(Order.SM, 2),
-					new BsColumn(new Label("3-rd, but 1-st on mobile"))
+					new BsColumn(new Span("3-rd, but 1-st on mobile"))
 						.withOrder(Order.XS, 1)
 						.withOrder(Order.SM, 3)
 				)
@@ -200,13 +198,13 @@ public class BsLayoutView extends VerticalLayout {
 			.withDefaultSize(Size.XS),
 			"new BsLayout(\n"
 			+ "	new BsRow(\n"
-			+ "		new BsColumn(new Label(\"1-st, but 3-rd on mobile\"))\n"
+			+ "		new BsColumn(new Span(\"1-st, but 3-rd on mobile\"))\n"
 			+ "			.withOrder(Order.XS, 3)\n"
 			+ "			.withOrder(Order.SM, 1),\n"
-			+ "		new BsColumn(new Label(\"2-nd\"))\n"
+			+ "		new BsColumn(new Span(\"2-nd\"))\n"
 			+ "			.withOrder(Order.XS, 2)\n"
 			+ "			.withOrder(Order.SM, 2),\n"
-			+ "		new BsColumn(new Label(\"3-rd, but 1-st on mobile\"))\n"
+			+ "		new BsColumn(new Span(\"3-rd, but 1-st on mobile\"))\n"
 			+ "			.withOrder(Order.XS, 1)\n"
 			+ "			.withOrder(Order.SM, 3)\n"
 			+ "	)\n"
